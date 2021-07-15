c871725094e568fda74837a2af4486f4083a1efc
vtonsor <30914395+vtonsor@users.noreply.github.com>
2021-07-15 17:08:01 -0500

    final commit

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -3,24 +3,25 @@ import java.util.Scanner;
 
 public class Area {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         double radius;
-        do {
-            System.out.println("What is the radius of the circle? ");
-            while (!sc.hasNextInt()) {
-                System.out.println("That's not a number!");
-                sc.next(); // this is important!
-            }
-            radius = sc.nextInt();
-        } while (radius <= 0);
-        Double area = Circle.getArea(radius);
-        System.out.println("The area of the circle is " + area + ".");
+        System.out.println("What is the radius of the circle? ");
+
+
+        if (!input.hasNextInt())
+            System.out.println("This is not a number!");
+
+        radius = input.nextDouble();
 
         while (radius < 0) {
             System.out.println("Please enter a positive number: ");
-            radius = sc.nextInt();
+            radius = input.nextDouble();
+
 
         }
+
+        Double area = Circle.getArea(radius);
+        System.out.println("The area of the circle is " + area + ".");
 
     }
 }
