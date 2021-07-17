01b33265d58a96f449ee36728b0d4dd728ee97b5
vtonsor <30914395+vtonsor@users.noreply.github.com>
2021-07-16 22:35:04 -0500

    studio counting characters initial commit

---
+++ src/org/launchcode/java/studios/countingcharacters/Quote.java
@@ -0,0 +1,9 @@
+package org.launchcode.java.studios.countingcharacters;
+
+public class Quote {
+    String str = "If the product of two terms is zero then common sense says at least one of the " +
+            "two terms has to be zero to start with. So if you move all the terms over to one side, " +
+            "you can put the quadratics into a form that can be factored allowing that side of the " +
+            "equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+
+}
