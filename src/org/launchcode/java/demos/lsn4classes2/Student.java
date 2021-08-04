3d109f97f5c8afb9e8e1f14ab6a5775c5b36b0d8
vtonsor <30914395+vtonsor@users.noreply.github.com>
2021-08-04 18:28:50 -0500

    chapter 5 exercises

--- src/org/launchcode/java/demos/lsn4classes2/Student.java
+++ src/org/launchcode/java/demos/lsn4classes2/Student.java
@@ -34,7 +34,7 @@ public class Student {
 
 
      //TODO: Uncomment and complete the getGradeLevel method here:
-    public static String getGradeLevel(int credits) {
+    public String getGradeLevel(int credits) {
         String gradeLevel;
         // Determine the grade level of the student based on numberOfCredits
         if (credits >= 90) {
